aed79d80be51df7eca1e27b3b733d6fc481dcec3
Scott Doxey <neogeek0101@gmail.com>
2015-08-11 21:13:09 -0400

    Updated packages.
    Switched to load-grunt-tasks for loading grunt packages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
 
     'use strict';
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
 
